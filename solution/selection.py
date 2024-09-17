3800907db7946eac1f9de68ab1cc09a776897a79
Jagoda Hanuszewicz <jagoda.hanuszewicz@gmail.com>
2024-09-17 14:03:05 +0200

    add parent selection function

--- solution/selection.py
+++ solution/selection.py
@@ -1,6 +1,20 @@
-# Selects 2 parents from the current population
-def parent_selection(population):
-    pass
+import random 
+
+# we need fitness to select parents:
+def fitness(individual):
+    # HERE: replace with our actual fitness evaluation
+    return individual['fitness']
+
+def tournament(population, k):
+    selected_individuals = random.sample(population, k)
+    best_individual = max(selected_individuals, key=fitness)
+    return best_individual
+
+# Selects 2 parents from the current population, choose k=3 or k=5
+def parent_selection(population, k):
+    p1 = tournament(population, k)
+    p2 = tournament(population, k)
+    return p1, p2
 
 # Updates the population by selecting survivors.
 def survivor_selection(population, offspring):
