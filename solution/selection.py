c3b89993df0b52c1b79c30fb2733ae4e423e9236
Jagoda Hanuszewicz <jagoda.hanuszewicz@gmail.com>
2024-09-17 14:16:11 +0200

    create parent selection function

--- solution/selection.py
+++ solution/selection.py
@@ -1,19 +1,21 @@
 import random 
 
-# we need fitness to select parents:
-def fitness(individual):
-    # HERE: replace with our actual fitness evaluation
-    return individual['fitness']
 
-def tournament(population, k):
+def fitness(env,x):
+    f,p,e,t = env.play(pcont=x)
+    return f
+
+def tournament(population, k, env):
     selected_individuals = random.sample(population, k)
-    best_individual = max(selected_individuals, key=fitness)
+    # Find the best individual among the selected based on fitness
+    best_individual = max(selected_individuals, key=lambda ind: fitness(env, ind['pcont']))
+    
     return best_individual
 
 # Selects 2 parents from the current population, choose k=3 or k=5
-def parent_selection(population, k):
-    p1 = tournament(population, k)
-    p2 = tournament(population, k)
+def parent_selection(population, k, env):
+    p1 = tournament(population, k, env)
+    p2 = tournament(population, k, env)
     return p1, p2
 
 # Updates the population by selecting survivors.
