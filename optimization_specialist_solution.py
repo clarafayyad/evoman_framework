38bd27dbfd53b290ca6c8ccb9f221a0cbd631e8c
Clara Fayyad <clarafayad@live.com>
2024-09-20 14:53:57 +0200

    Separate train & test

---
+++ optimization_specialist_solution.py
@@ -0,0 +1,33 @@
+# imports
+import optimization_specialist_test
+import optimization_specialist_train
+from demo_controller import player_controller
+from evoman.environment import Environment
+import reporting
+import time
+
+# NN configuration
+hidden_neurons = 10
+
+# Set experiment name
+experiment = 'experiments'
+
+# Initialize simulation
+env = Environment(experiment_name=experiment,
+                  enemies=[8],
+                  playermode="ai",
+                  player_controller=player_controller(hidden_neurons),
+                  enemymode="static",
+                  level=2,
+                  speed="fastest",
+                  visuals=False)
+
+is_test = False
+ini_time = reporting.start_experiment(experiment, is_test)
+
+if is_test:
+    optimization_specialist_test.test_experiment(experiment, env)
+else:
+    optimization_specialist_train.train_experiment(experiment, env, hidden_neurons)
+
+reporting.log_execution_time(time.time() - ini_time)
--- optimization_specialist_train.py
+++ optimization_specialist_train.py
@@ -1,15 +1,11 @@
 # imports
-import optimization_specialist_test
-from demo_controller import player_controller
-from evoman.environment import Environment
-import time
 import reporting
 import stats
 import operators
 
 # Define a set of constants
 POPULATION_SIZE = 100
-TOTAL_GENERATIONS = 10
+TOTAL_GENERATIONS = 30
 
 # Set EA Operators Parameters
 lower_bound = -1
@@ -19,86 +15,59 @@ mutation_rate = 0.2
 mutation_sigma = 0.1
 selection_pressure = 1.2
 
-# NN configuration
-hidden_neurons = 10
 
-# Set experiment name
-experiment = 'experiments'
+def train_experiment(experiment, env, hidden_neurons):
+    # Compute individual size
+    individual_size = (env.get_num_sensors() + 1) * hidden_neurons + (hidden_neurons + 1) * 5
 
-# Initialize simulation
-env = Environment(experiment_name=experiment,
-                  enemies=[8],
-                  playermode="ai",
-                  player_controller=player_controller(hidden_neurons),
-                  enemymode="static",
-                  level=2,
-                  speed="fastest",
-                  visuals=False)
+    # Log the environment state
+    env.state_to_log()
 
-# Start experiment
-is_test = False
-ini_time = reporting.start_experiment(experiment, is_test)
-if is_test:
-    optimization_specialist_test.test_experiment(experiment, env)
+    # Initialize generation counter
+    generation_number = 0
 
+    # Initialize population
+    population = operators.initialize_population(POPULATION_SIZE, individual_size, lower_bound, upper_bound)
+    fitness_values = operators.evaluate_population(env, population)
 
-# Compute individual size
-individual_size = (env.get_num_sensors()+1)*hidden_neurons + (hidden_neurons+1)*5
-
-# Log the environment state
-env.state_to_log()
-
-# Initialize generation counter
-generation_number = 0
-
-# Initialize population
-population = operators.initialize_population(POPULATION_SIZE, individual_size, lower_bound, upper_bound)
-fitness_values = operators.evaluate_population(env, population)
-
-# Save current population and fitness values
-env.update_solutions([population, fitness_values])
-
-# Compute and log stats
-best_individual_index, mean, std = stats.compute_stats(fitness_values)
-reporting.log_stats(experiment, generation_number, fitness_values[best_individual_index], mean, std)
-
-
-# Evolution
-while generation_number < TOTAL_GENERATIONS:
-    # Create offspring
-    tournament_count = int(POPULATION_SIZE / 2)
-    offspring = operators.random_arithmetic_crossover(population, fitness_values, tournament_count, tournament_size)
-
-    # Mutate offspring
-    for i in range(len(offspring)):
-        offspring[i] = operators.gaussian_mutation(offspring[i], rate=mutation_rate, sigma=mutation_sigma)
-
-    # Evaluate offspring
-    offspring_fitness = operators.evaluate_population(env, offspring)
-
-    # Survivor selection
-    population, fitness_values = operators.linear_ranking_survivor_selection(
-        population,
-        fitness_values,
-        offspring,
-        offspring_fitness,
-        s=selection_pressure)
+    # Save current population and fitness values
+    env.update_solutions([population, fitness_values])
 
     # Compute and log stats
     best_individual_index, mean, std = stats.compute_stats(fitness_values)
     reporting.log_stats(experiment, generation_number, fitness_values[best_individual_index], mean, std)
 
-    # Save file with the best solution
-    reporting.save_best_individual(experiment, population[best_individual_index])
+    # Evolution
+    while generation_number < TOTAL_GENERATIONS:
+        # Create offspring
+        tournament_count = int(POPULATION_SIZE / 2)
+        offspring = operators.random_arithmetic_crossover(population, fitness_values, tournament_count, tournament_size)
 
-    # Update and save simulation state
-    env.update_solutions([population, fitness_values])
-    env.save_state()
+        # Mutate offspring
+        for i in range(len(offspring)):
+            offspring[i] = operators.gaussian_mutation(offspring[i], rate=mutation_rate, sigma=mutation_sigma)
 
-    generation_number += 1
+        # Evaluate offspring
+        offspring_fitness = operators.evaluate_population(env, offspring)
 
-# Print total execution time
-reporting.log_execution_time(time.time() - ini_time)
+        # Survivor selection
+        population, fitness_values = operators.linear_ranking_survivor_selection(
+            population,
+            fitness_values,
+            offspring,
+            offspring_fitness,
+            s=selection_pressure)
 
-# Log environment state
-# env.state_to_log()
+        # Compute and log stats
+        best_individual_index, mean, std = stats.compute_stats(fitness_values)
+        reporting.log_stats(experiment, generation_number, fitness_values[best_individual_index], mean, std)
+
+        # Save file with the best solution
+        reporting.save_best_individual(experiment, population[best_individual_index])
+
+        # Update and save simulation state
+        env.update_solutions([population, fitness_values])
+        env.save_state()
+
+        generation_number += 1
+
